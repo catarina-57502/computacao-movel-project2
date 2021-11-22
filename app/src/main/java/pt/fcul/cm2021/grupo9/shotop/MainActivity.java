38cf6b5fa472d7b287a48e64d81a7570e7d7892e
Martim Mourao <martimmourao@gmail.com>
2021-11-22 18:42:35 +0000

    Splash Screen e logo

---
+++ app/src/main/java/pt/fcul/cm2021/grupo9/shotop/MainActivity.java
@@ -0,0 +1,17 @@
+package pt.fcul.cm2021.grupo9.shotop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/pt/fcul/cm2021/grupo9/shotop/SplashActivity.java
@@ -0,0 +1,28 @@
+package pt.fcul.cm2021.grupo9.shotop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    /** Duração Splash Screen **/
+    private final int SPLASH_TIME = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_TIME);
+    }
+}
