d7af8ef2f96743e694dcaa244ad7a5c0391a6d56
catarina-57502 <fc57502@alunos.fc.ul.pt>
2022-01-19 15:42:57 +0000

    alguns ajustes UI e tags para as características

--- app/src/main/java/pt/fcul/cm2021/grupo9/shotop/MySpots/SpotInfoFragment.java
+++ app/src/main/java/pt/fcul/cm2021/grupo9/shotop/MySpots/SpotInfoFragment.java
@@ -1,5 +1,6 @@
 package pt.fcul.cm2021.grupo9.shotop.MySpots;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -7,6 +8,7 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -17,10 +19,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 import java.util.Locale;
 
+import okqapps.com.tagslayout.TagItem;
+import okqapps.com.tagslayout.TagTextSize;
+import okqapps.com.tagslayout.TagsLayout;
 import pt.fcul.cm2021.grupo9.shotop.R;
 import pt.fcul.cm2021.grupo9.shotop.desafio.DesafioCamerasFragment;
 import pt.fcul.cm2021.grupo9.shotop.entidades.Spot;
@@ -34,6 +40,7 @@ public class SpotInfoFragment extends Fragment {
         this.spot = spot;
     }
 
+    @SuppressLint("ResourceType")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -91,11 +98,18 @@ public class SpotInfoFragment extends Fragment {
             }
         }
 
-        TextView t3 = v.findViewById(R.id.caracSpot);
-        if (spot.getCaracteristicas() == null) {
-            t3.setText("N/A");
-        } else {
-            t3.setText(spot.getCaracteristicas().toString());
+        TagsLayout t3 = v.findViewById(R.id.caracSpot);
+        if (spot.getCaracteristicas() != null) {
+            List<TagItem> tagItems = new ArrayList<>();
+            int i = 1;
+            for(String c: spot.getCaracteristicas()){
+                tagItems.add(new TagItem(i, c, getResources().getString(R.color.blue), getResources().getString(R.color.white), true));
+                i++;
+            }
+            t3.setViewMode(true);
+            t3.setTagTextSize(TagTextSize.SMALL);
+            t3.initializeTags(getActivity(), tagItems);
+
         }
 
         TextView t4 = v.findViewById(R.id.imageHeightSpot);
